c4277035cb97e3a7a4084fcbcb86740482edde66
jaeeunoh <jaeeunnooh@gmail.com>
2026-04-09 21:00:22 +0900

    fix: 주석 처리

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from modules.open_cv import process_video
 from ultralytics import YOLO
 
 def main():
-    video_path = "LG_4_문보경.mp4" # 영상 1개의 경로 입력
+    video_path = "" # 영상 1개의 경로 입력
     yolo_model_path = "weights/custom_yolo_model.pt"
     yolo_model = YOLO(yolo_model_path)
     
